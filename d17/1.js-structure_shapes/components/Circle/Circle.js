0c3fe90499eb920dc6acde9a7bbc49eff6951ebf
Xaver Schüll <botacode@gmail.com>
2023-03-14 14:59:58 +0100

    Challenge 1: Submission

---
+++ d17/1.js-structure_shapes/components/Circle/Circle.js
@@ -0,0 +1,14 @@
+console.log("Circle.js intialized ⚪️");
+
+import { getRandomColor } from "../../utils/randomColor.js";
+
+const Circle = () => {
+  const circle = document.createElement("div");
+  circle.classList.add("circle");
+  circle.addEventListener("click", () => {
+    circle.style.backgroundColor = getRandomColor();
+  });
+  return circle;
+};
+
+export default Circle;
