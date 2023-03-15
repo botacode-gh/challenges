77d828ddbddbaebd276e4985d1fbe2e1f3a113eb
Xaver Schüll <botacode@gmail.com>
2023-03-15 17:07:51 +0100

    Challenge PM 1: Submission

--- d18/PM.1.js-fetch_star-wars-console/js/index.js
+++ d18/PM.1.js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,21 @@
 console.clear();
+console.log("index.js initialized 🏠");
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+const root = document.getElementById("root");
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(url);
+    console.log("Fetched data from", url);
+    const JSONData = await response.json();
+    console.log(JSONData);
+    const r2d2 = JSONData.results.find((char) => char.name === "R2-D2");
+    console.log(r2d2.eye_color);
+  } catch {
+    console.log("Something went wrong :(");
+  }
+};
 
 fetchData();
